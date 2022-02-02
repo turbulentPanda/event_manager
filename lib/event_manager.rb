a95756209220b27e8b35a68224895ede9447da4b
Christopher Marchant <cmarch240@gmail.com>
2022-02-02 14:00:34 -0600

    Created clean_zipcode method to standardize zipcode format

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,4 +1,17 @@
 require 'csv'
+
+def clean_zipcode(zipcode)
+  if zipcode.nil?
+    '00000'
+  elsif zipcode.length < 5
+    zipcode.rjust(5, '0')
+  elsif zipcode.length > 5
+    zipcode[0..4]
+  else
+    zipcode
+  end
+end
+
 puts 'EventManager initialized'
 
 contents = CSV.open(
@@ -9,6 +22,8 @@ contents = CSV.open(
 
 contents.each do |row|
   name = row[:first_name]
-  zipcode = row[:zipcode]
+  
+  zipcode = clean_zipcode(row[:zipcode])
+
   puts "#{name} #{zipcode}"
 end
