82f578ac8ac39cb93ceb3dd1a4970d299111488f
Christopher Marchant <cmarch240@gmail.com>
2022-02-02 16:51:11 -0600

    Created save_thank_you_letter method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -21,6 +21,17 @@ def legislators_by_zipcode(zipcode)
   end
 end
 
+def save_thank_you_letter(id, form_letter)
+  Dir.mkdir('poutut') unless Dir.exist?('output')
+
+  filename = "output/thanks_#{id}.html"
+
+  File.open(filname, 'w') do |file|
+    file.puts form_letter
+  end
+end
+
+
 puts 'EventManager initialized'
 
 
@@ -34,6 +45,7 @@ template_letter = File.read('form_letter.erb')
 erb_template = ERB.new template_letter
 
 contents.each do |row|
+  id = row[0]
   name = row[:first_name]
   
   zipcode = clean_zipcode(row[:zipcode])
@@ -41,7 +53,7 @@ contents.each do |row|
   legislators = legislators_by_zipcode(zipcode)
 
   form_letter = erb_template.result(binding)
-  puts form_letter
 
-  puts form_letter
+  save_thank_you_letter(id, form_letter)
+
 end
