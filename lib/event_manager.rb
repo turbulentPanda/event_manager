2a3af3fd42c30cf3ed2859c634ea5713d944e981
Christopher Marchant <cmarch240@gmail.com>
2022-02-02 17:09:33 -0600

    Created clean_phone_number method

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,16 @@ def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
 
+def clean_phone_number(phone_number)
+  if phone_number.length == 10
+    phone_number
+  elsif phone_number.length == 11 && phone_number[0] == '1'
+    phone_number[1..-1]
+  else
+    nil
+  end
+end
+
 def legislators_by_zipcode(zipcode)
   civic_info = Google::Apis::CivicinfoV2::CivicInfoService.new
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
