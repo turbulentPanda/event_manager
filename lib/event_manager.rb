4e7ce9d2fdd51524004b20278a58575b0c41d4f5
Christopher Marchant <cmarch240@gmail.com>
2022-02-02 15:04:19 -0600

    Adjusted code so that only the names of the legislators are printed by the attendee

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -28,9 +28,13 @@ contents.each do |row|
       roles: ['legislatorUpperBody', 'legislatorLowerBody']
     )
     legislators = legislators.officials
+
+    legislator_names = legislators.map(&:name)
+
+    legislators_string = legislator_names.join(", ")
   rescue
-    'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
+    legislators_string = 'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
   end
 
-  puts "#{name} #{zipcode} #{legislators}"
+  puts "#{name} #{zipcode} #{legislators_string}"
 end
